1fc66b0c9772f29fbc5c5923760f4bcdada003e3
nahjd <nahidmemmedov711@gmail.com>
2025-07-18 12:58:58 +0400

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,44 +3,51 @@ import { Link } from 'react-router-dom';
 import './Navbar.scss';
 import { ThemeContext } from '../../src/context/ThemeContext';
 import { LanguageContext } from '../../src/context/LanguageContext';
+import { RiMenuFill, RiCloseFill } from 'react-icons/ri';
 
 export default function Navbar() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
   const { language, changeLanguage, t } = useContext(LanguageContext);
   const [menuOpen, setMenuOpen] = useState(false);
   const [search, setSearch] = useState('');
 
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
-    <nav className={`navbar-container ${theme}`}>
-      <div className="navbar-logo">
-        <Link to="/">MyApp</Link>
-      </div>
-
-      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <ul>
-          <li><Link to="/home">{t('home')}</Link></li>
-          <li><Link to="/blog">{t('blog')}</Link></li>
-          <li><Link to="/about">{t('about')}</Link></li>
-          <li><Link to="/login" className="btn btn-login">{t('signIn')}</Link></li>
-          <li><Link to="/register" className="btn btn-signup">{t('signUp')}</Link></li>
-        </ul>
-        <div className="navbar-actions">
-          <input 
-            type="text" 
-            placeholder={t('search')} 
-            value={search} 
-            onChange={(e) => setSearch(e.target.value)} 
-          />
-          <select onChange={(e) => changeLanguage(e.target.value)} value={language}>
-            <option value="az">AZ</option>
-            <option value="tr">TR</option>
-            <option value="en">EN</option>
-          </select>
+    <nav className={`navbar ${theme}`}>
+      <div className="navbar-content">
+        <div className="navbar-left">
+          <Link to="/" className="logo" onClick={closeMenu}>MyApp</Link>
         </div>
-      </div>
 
-      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        {menuOpen ? '✖' : '☰'}
+        <button className="hamburger" onClick={toggleMenu}>
+          {menuOpen ? <RiCloseFill /> : <RiMenuFill />}
+        </button>
+
+        <div className={`navbar-right ${menuOpen ? 'active' : ''}`}>
+          <ul className="nav-links">
+            <li><Link to="/home" onClick={closeMenu}>{t('home')}</Link></li>
+            <li><Link to="/blog" onClick={closeMenu}>{t('blog')}</Link></li>
+            <li><Link to="/about" onClick={closeMenu}>{t('about')}</Link></li>
+            <li><Link to="/login" className="btn login" onClick={closeMenu}>{t('signIn')}</Link></li>
+            <li><Link to="/register" className="btn signup" onClick={closeMenu}>{t('signUp')}</Link></li>
+          </ul>
+
+          <div className="actions">
+            <input
+              type="text"
+              placeholder={t('search')}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <select value={language} onChange={(e) => changeLanguage(e.target.value)}>
+              <option value="az">AZ</option>
+              <option value="tr">TR</option>
+              <option value="en">EN</option>
+            </select>
+          </div>
+        </div>
       </div>
     </nav>
   );
